dbe77e3ce99713b7417b275c984855c34ece0b0c
IvanPs5 <ivan2000.ps@gmail.com>
2021-12-02 10:31:33 +0100

    3 files modified

--- script.js
+++ script.js
@@ -1,42 +1,64 @@
 $(document).ready(function () {
-  $('.row__hover-button').click(function () {
+  $(document).on('click', '.todo-component__button--delete', function () {
     let parent = $(this).parent();
     parent.remove();
+    updateFooterText();
   });
 
-  $('.addon__button').click(function () {
+  $('.todo-component__button--add').click(function () {
     let list = $('.todo-component__list');
     let line = document.createElement('li');
-    line.className = 'list__row';
+    line.className = 'todo-component__list-row';
     let textDiv = document.createElement('div');
-    textDiv.className = 'row__text';
+    textDiv.className = 'todo-component__list-row-text';
     let checkbox = document.createElement('input');
     let text = document.createElement('span');
     checkbox.type = 'checkbox';
-    checkbox.className = 'text__checkbox';
-    let inputText = $('#addNewTodo').val();
+    checkbox.className = 'todo-component__checkbox';
+    let inputText = $('#add-new-todo').val();
     text.innerHTML = inputText;
     textDiv.append(checkbox);
     textDiv.append(text);
     let buttonDelete = document.createElement('button');
-    buttonDelete.className = 'row__hover-button';
+    buttonDelete.className =
+      'todo-component__button todo-component__button--delete';
     let iconButton = document.createElement('i');
     iconButton.className = 'fas fa-trash';
     buttonDelete.append(iconButton);
     line.append(textDiv);
     line.append(buttonDelete);
     list.append(line);
+    updateFooterText();
   });
 
-  $('.footer__button').click(function () {
+  $('.todo-component__button--clear').click(function () {
     let list = $('.todo-component__list');
     list.empty();
+    updateFooterText();
   });
-});
 
-$(document).on(function () {
-  $('.row__hover-button').click(function () {
-    let parent = $(this).parent();
-    parent.remove();
+  function updateFooterText() {
+    let sizeList =
+      $('.todo-component__list').children().length -
+      howManyCheckboxAreChecked();
+    let footerText;
+    if (sizeList === 1) {
+      footerText = 'You have ' + sizeList + ' pending task.';
+    } else {
+      footerText = 'You have ' + sizeList + ' pending tasks.';
+    }
+    document.getElementById('text_pending_task').innerHTML = footerText;
+  }
+
+  function howManyCheckboxAreChecked() {
+    let checkboxesChecked = 0;
+    $('.todo-component__checkbox:checked').each(function () {
+      checkboxesChecked++;
+    });
+    return checkboxesChecked;
+  }
+
+  $(document).on('click', '.todo-component__checkbox', function () {
+    updateFooterText();
   });
 });
