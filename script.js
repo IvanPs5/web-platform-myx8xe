82ce7c408de4ea552356118a919b70da6a472c22
IvanPs5 <ivan2000.ps@gmail.com>
2021-12-02 11:25:58 +0100

    3 files modified

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
     checkbox.className = 'todo-component__checkbox';
     let inputText = $('#add-new-todo').val();
     text.innerHTML = inputText;
+    text.className = 'todo-component__text';
     textDiv.append(checkbox);
     textDiv.append(text);
     let buttonDelete = document.createElement('button');
@@ -31,7 +32,7 @@ $(document).ready(function () {
     updateFooterText();
   });
 
-  $('.todo-component__button--clear').click(function () {
+  $('#clear-button').click(function () {
     let list = $('.todo-component__list');
     list.empty();
     updateFooterText();
@@ -60,5 +61,32 @@ $(document).ready(function () {
 
   $(document).on('click', '.todo-component__checkbox', function () {
     updateFooterText();
+  });
+
+  $('#button-show-all-tasks').click(function () {
+    showAllTasks();
+  });
+  function showAllTasks() {
+    $('.todo-component__checkbox').each(function () {
+      let task = $(this).parent().parent();
+      task.show();
+    });
+  }
+  $('#button-show-completed-tasks').click(function () {
+    showAllTasks();
+    $('.todo-component__checkbox').each(function () {
+      if (!$(this).prop('checked')) {
+        let taskPending = $(this).parent().parent();
+        taskPending.hide();
+      }
+    });
+  });
+
+  $('#button-show-pending-tasks').click(function () {
+    showAllTasks();
+    $('.todo-component__checkbox:checked').each(function () {
+      let taskCompleted = $(this).parent().parent();
+      taskCompleted.hide();
+    });
   });
 });
