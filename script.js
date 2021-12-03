34a343fe2ee61ffdc693102307300ca7698495ae
IvanPs5 <ivan2000.ps@gmail.com>
2021-12-03 12:17:11 +0100

    2 files modified

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-  $('.todo-component__list').on('click', '.btn--large-delete', function () {
-    removeListRow($(this).parent());
-    updateFooterText();
-  });
-
   $('.btn.btn--large-add').click(function () {
     createListRow();
     updateFooterText();
@@ -13,15 +8,6 @@ $(document).ready(function () {
     clearList();
     updateFooterText();
   });
-
-  $('.todo-component__list').on(
-    'click',
-    '.todo-component__checkbox',
-    function () {
-      activateCheckbox($(this));
-      updateFooterText();
-    }
-  );
 
   $('#button-show-all-tasks').click(function () {
     showTasks('all');
@@ -35,14 +21,17 @@ $(document).ready(function () {
     showTasks('pending');
   });
 
-  $('.todo-component__list').on(
-    'click',
-    '.todo-component__list-row-text',
-    function () {
-      activateCheckbox($(this).find('input'));
-      updateFooterText();
-    }
-  );
+  $('.btn--large-delete').on('click', function () {
+    removeListRow($(this).parent());
+  });
+
+  $('.todo-component__checkbox').on('click', function () {
+    activateCheckbox($(this));
+  });
+
+  $('.todo-component__list-row-text').on('click', function () {
+    activateCheckbox($(this).find('input'));
+  });
 
   function activateCheckbox(checkbox) {
     let valueChecked = true;
@@ -50,20 +39,28 @@ $(document).ready(function () {
       valueChecked = false;
     }
     checkbox.prop('checked', valueChecked);
+    updateFooterText();
   }
 
   function removeListRow(element) {
     element.remove();
+    updateFooterText();
   }
   function createListRow() {
     let list = $('.todo-component__list');
 
     let row = $('<li>').attr('class', 'todo-component__list-row');
+    row.on('click', function () {
+      activateCheckbox($(this).find('input'));
+    });
 
     let textDiv = $('<div>').attr('class', 'todo-component__list-row-text');
     let checkbox = $('<input>')
       .attr('class', 'todo-component__checkbox')
       .attr('type', 'checkbox');
+    checkbox.on('click', function () {
+      activateCheckbox($(this));
+    });
     let text = $('<label>')
       .attr('class', 'todo-component__text')
       .text($('#add-new-todo').val());
@@ -73,6 +70,9 @@ $(document).ready(function () {
     let buttonDelete = $('<button>').attr('class', 'btn btn--large-delete');
     let iconButton = $('<i>').attr('class', 'fas fa-trash');
     buttonDelete.append(iconButton);
+    buttonDelete.on('click', function () {
+      removeListRow($(this).parent());
+    });
 
     row.append(textDiv);
     row.append(buttonDelete);
@@ -103,17 +103,16 @@ $(document).ready(function () {
   }
 
   function showTasks(target) {
-    var checkboxTarget = '.todo-component__checkbox';
-    var isCompleted = target === 'completed';
-    var isPending = target === 'pending';
+    let isCompletedTasks = target === 'completed';
+    let isPendingTasks = target === 'pending';
 
-    $(checkboxTarget).each(function () {
+    $('.todo-component__checkbox').each(function () {
       let task = $(this).parent().parent();
       task.show();
-      if (isCompleted && $(this).prop('checked')) {
+      if (isCompletedTasks && $(this).prop('checked')) {
         return true;
       }
-      if (isCompleted || (isPending && $(this).prop('checked'))) {
+      if (isCompletedTasks || (isPendingTasks && $(this).prop('checked'))) {
         task.hide();
       }
     });
