2f9aee2c4e86e604a73b2e95dae07c0444fd44e1
IvanPs5 <ivan2000.ps@gmail.com>
2021-12-14 10:14:51 +0100

    1 file modified

--- script.js
+++ script.js
@@ -41,7 +41,9 @@ $(document).ready(function () {
 
   function isSafeText(text) {
     let returnValue = true;
-
+    if (text.indexOf('<script>') > -1) {
+      returnValue = false;
+    }
     return returnValue;
   }
 
