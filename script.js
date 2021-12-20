d6f0ac7fd53b100abe0d77015ce6fc8a3975b381
IvanPs5 <ivan2000.ps@gmail.com>
2021-12-20 12:47:20 +0100

    2 files modified

--- script.js
+++ script.js
@@ -5,26 +5,38 @@ $(document).ready(function () {
   });
 
   $('.todo-component__list_button').click(function () {
-    const todoComponent = $(this).parents('.todo-component');
-    todoComponent.removeClass('todo-component--showing-completed');
-    todoComponent.removeClass('todo-component--showing-pending');
+    showAllTasks($(this));
   });
 
   $('.todo-component__list_button--completed').click(function () {
-    const todoComponent = $(this).parents('.todo-component');
-    todoComponent.removeClass('todo-component--showing-pending');
-    todoComponent.addClass('todo-component--showing-completed');
+    showCompletedTasks($(this));
   });
 
   $('.todo-component__list_button--pending').click(function () {
-    const todoComponent = $(this).parents('.todo-component');
-    todoComponent.removeClass('todo-component--showing-completed');
-    todoComponent.addClass('todo-component--showing-pending');
+    showPendingTasks($(this));
   });
 
   $('.btn--large-delete').on('click', function () {
     removeListRow($(this).parents('.todo-component__list-row'));
   });
+
+  function showAllTasks(target) {
+    const todoComponent = target.parents('.todo-component');
+    todoComponent.removeClass('todo-component--showing-completed');
+    todoComponent.removeClass('todo-component--showing-pending');
+  }
+
+  function showCompletedTasks(target) {
+    const todoComponent = target.parents('.todo-component');
+    todoComponent.removeClass('todo-component--showing-pending');
+    todoComponent.addClass('todo-component--showing-completed');
+  }
+
+  function showPendingTasks(target) {
+    const todoComponent = target.parents('.todo-component');
+    todoComponent.removeClass('todo-component--showing-completed');
+    todoComponent.addClass('todo-component--showing-pending');
+  }
 
   function activateCheckbox(checkbox) {
     let valueChecked = true;
@@ -147,27 +159,24 @@ $(document).ready(function () {
     $.widget('custom.inputTasks', {
       options: {},
       _create: function () {
-        this.element.addClass('todo-component__input-area');
-        const form = $('<form/>', {
+        const formElm = $('<form/>', {
           action: '',
           class: 'todo-component__form',
         }).appendTo(this.element);
-        const div = $('<div/>', {
+        const divElm = $('<div/>', {
           class: 'todo-component__input-area',
-        }).appendTo(form);
-        const input = $('<input>', {
+        }).appendTo(formElm);
+        const inputElm = $('<input>', {
           class: 'todo-component__input',
           placeholder: 'Add your new todo',
-        }).appendTo(div);
-        const btn = $('<button/>', {
+        }).appendTo(divElm);
+        const btnElm = $('<button/>', {
           type: 'submit',
           text: '+',
           class: 'btn btn--large',
-        }).appendTo(div);
+        }).appendTo(divElm);
       },
       _setOption: function (key, value) {
-        if (key === 'value') {
-        }
         this._super(key, value);
       },
       _setOptions: function (options) {
@@ -177,7 +186,86 @@ $(document).ready(function () {
         this.element.removeClass('todo-component__input-area').text('');
       },
     });
-    $('#my-widget1').inputTasks();
+    $('.todo-component__widget-input').inputTasks();
+
+    $.widget('custom.listButtonsOrganizers', {
+      options: {},
+      _create: function () {
+        this.element.addClass('todo-component__buttons-organizers');
+        const ulElm = $('<ul/>', {
+          class: 'todo-component__list-buttons',
+        }).appendTo(this.element);
+        ulElm.append(
+          this._createNewLiWithBtn(
+            'todo-component__list_button',
+            'fas fa-th-list'
+          )
+        );
+
+        ulElm.append(
+          this._createNewLiWithBtn(
+            'todo-component__list_button todo-component__list_button--completed',
+            'fas fa-tasks'
+          )
+        );
+
+        ulElm.append(
+          this._createNewLiWithBtn(
+            'todo-component__list_button todo-component__list_button--pending',
+            'fas fa-list'
+          )
+        );
+      },
+
+      _setOptions: function (options) {
+        this._super(options);
+      },
+      _createNewLiWithBtn: function (classNameBtn, classNameBtnIcon) {
+        const btnElm = this._createNewBtn(classNameBtn, classNameBtnIcon);
+        const liElm = $('<li/>', {
+          class: 'todo-component__list-buttons-row',
+        });
+        btnElm.appendTo(liElm);
+        return liElm;
+      },
+      _createNewBtn: function (className, classNameIcon) {
+        const btnElm = $('<button/>', {
+          class: '' + className,
+          click: function () {
+            showAllTasks(btnElm);
+          },
+        });
+        if (
+          className ===
+          'todo-component__list_button todo-component__list_button--completed'
+        ) {
+          btnElm.click(function () {
+            showCompletedTasks(btnElm);
+          });
+        }
+
+        if (
+          className ===
+          'todo-component__list_button todo-component__list_button--pending'
+        ) {
+          btnElm.click(function () {
+            showPendingTasks(btnElm);
+          });
+        }
+
+        const iElm = $('<i/>', {
+          class: '' + classNameIcon,
+        }).appendTo(btnElm);
+        return btnElm;
+      },
+      _destroy: function () {
+        this.element.removeClass('todo-component__buttons-organizers').text('');
+      },
+    });
+
+    $(
+      '.todo-component__widget-list-buttons-organizers'
+    ).listButtonsOrganizers();
 
     $('.todo-component__form').on('submit', function (event) {
       event.preventDefault();
